b948fbd961108f7489974aff016c7114102822c8
SayakDutta01 <84614982+SayakDutta01@users.noreply.github.com>
2022-04-25 20:25:15 +0530

    Update test-file.sh

--- Scripts/test-file.sh
+++ Scripts/test-file.sh
@@ -4,23 +4,23 @@ echo "Hey Enter the File/Directory name (using the absolute path)?"
 read FILE
 
 if [ -e "$FILE" ]; then
-	  if [ -f "$FILE" ]; then
-		  echo "$FILE is a regular file."
-	  fi
-	  if [ -d "$FILE" ]; then
-		  echo "$FILE is a directory."
-	  fi
-	  if [ -r "$FILE" ]; then
-		  echo "$FILE is readable."
-	  fi
-	  if [ -w "$FILE" ]; then
-		  echo "$FILE is writable."
-	  fi
-	  if [ -x "$FILE" ]; then
-		  echo "$FILE is executable/searchable."
-	  fi
+	if [ -f "$FILE" ]; then
+		echo "$FILE is a regular file."
+	fi
+	if [ -d "$FILE" ]; then
+		echo "$FILE is a directory."
+	fi
+	if [ -r "$FILE" ]; then
+		echo "$FILE is readable."
+	fi
+	if [ -w "$FILE" ]; then
+		echo "$FILE is writable."
+	fi
+	if [ -x "$FILE" ]; then
+		echo "$FILE is executable/searchable."
+	fi
 else
-	  echo "$FILE does not exist"
-	  exit 1
+	echo "$FILE does not exist"
+	exit 1
 fi
 exit
